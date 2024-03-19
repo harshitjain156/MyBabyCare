06f209064366aee74949c8478d4473be3573d51a
Gaurav Verma <gaurav.v@codestoresolutions.com>
2024-03-19 13:54:47 +0530

    child delete api endpoint created.

--- server/routes/childRouter.js
+++ server/routes/childRouter.js
@@ -1,8 +1,9 @@
 const express = require( 'express' );
 const router = express.Router();
-const {addNewChild, getAllChild} = require("../controller/childController");
+const {addNewChild, getAllChild, deleteChild} = require("../controller/childController");
 // Require controller modules.
 router.post("/add-new-child", addNewChild);
-router.get("/all-child", getAllChild)
+router.get("/all-child", getAllChild);
+router.delete("/delete/:id", deleteChild)
 
 module.exports = router;
